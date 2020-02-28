175ca60096a54423872d9b819719cbd3b417011f
Davi Arndt <davi-arndt@outlook.com>
2020-02-28 20:03:35 -0300

    Refatora código

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("Valor"),
-                subtitle: Text("Número da Conta"),
-                dense: false,
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(
           title: Text("Transferências"),
         ),
@@ -23,3 +12,40 @@ void main() => runApp(MaterialApp(
         ),
       ),
     ));
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 11111)),
+        ItemTransferencia(Transferencia(13200.0, 22222)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+        dense: false,
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
