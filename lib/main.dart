05140f9a7ce8e81f273f099406f9e2a8d3f4d202
Davi Arndt <davi-arndt@outlook.com>
2020-02-28 20:47:01 -0300

    Refatoração de código e adiciona formulário

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,70 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
-        appBar: AppBar(
-          title: Text("Transferências"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
+void main() => runApp(BytebankApp());
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  labelText: 'Número da Conta',
+                  hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0.00'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+              color: Colors.blue,
+              onPressed: () {},
+              textColor: Colors.white,
+              child: Text("Confirmar")),
+        ],
       ),
-    ));
+      appBar: AppBar(
+        title: Text("Criando Transferência"),
+      ),
+    );
+  }
+}
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0, 11111)),
-        ItemTransferencia(Transferencia(13200.0, 22222)),
-      ],
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 11111)),
+          ItemTransferencia(Transferencia(13200.0, 22222)),
+        ],
+      ),
+      appBar: AppBar(
+        title: Text("Transferências"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -48,4 +92,15 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
 }
